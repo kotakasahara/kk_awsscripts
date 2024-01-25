beaf1802bf65993f2feaac94dc028cff003c7b12
Kota Kasahara <kota@precious.local>
2024-01-25 16:14:52 +0900

    v.0.04

--- postinstall_docker.sh
+++ postinstall_docker.sh
@@ -42,8 +42,8 @@ elif [ "${OS}" = "Ubuntu" ]; then
 	&& curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
 	    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
 	    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
-    apt-get update
-    apt-get install -y nvidia-container-toolkit
+    sudo apt-get update
+    sudo apt-get install -y nvidia-container-toolkit
     ##
 
     systemctl enable docker.service
